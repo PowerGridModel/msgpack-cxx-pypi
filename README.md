2d8014ed1825fa919e891f050ccd664673000448
Martijn Govers <Martijn.Govers@Alliander.com>
2023-09-19 10:01:24 +0200

    attempt build package
    Signed-off-by: Martijn Govers <Martijn.Govers@Alliander.com>

--- README.md
+++ README.md
@@ -9,3 +9,10 @@ This repo is the equivalent of the [Anaconda msgpack-cxx](https://anaconda.org/c
 The Python package can be found in [msgpack](https://pypi.org/project/msgpack/).
 
 Credits go to the maintainers of the [msgpack-cxx](https://github.com/msgpack/msgpack-c/tree/cpp_master) package.
+
+## License
+
+The files in this repository are licensed under the license listed in [LICENSE](./LICENSE).
+The distributed msgpack-cxx package is a redistribution of [msgpack-cxx](https://github.com/msgpack/msgpack-c/tree/cpp_master) and therefore falls under the [msgpack-cxx license model](https://github.com/msgpack/msgpack-c/blob/cpp_master/COPYING) as described in the [COPYING file in the msgpack-cxx](https://github.com/msgpack/msgpack-c/blob/cpp_master/COPYING).
+Since all distributed files are obtained by download in the automation, instead of being explicitly included into this library,
+the [msgpack-cxx license](https://github.com/msgpack/msgpack-c/blob/cpp_master/COPYING) is not provided explicitly as a file in this repository either.
---
+++ setup.py
@@ -0,0 +1,77 @@
+# SPDX-FileCopyrightText: 2022 Contributors to the Power Grid Model project <dynamic.grid.calculation@alliander.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+import os
+import platform
+import shutil
+from itertools import chain
+from pathlib import Path
+from typing import List
+
+# noinspection PyPackageRequirements
+from setuptools import setup
+from setuptools.command.build_ext import build_ext
+from wheel.bdist_wheel import bdist_wheel
+
+
+class bdist_wheel_abi_none(bdist_wheel):
+    def finalize_options(self):
+        bdist_wheel.finalize_options(self)
+        self.root_is_pure = False
+
+    def get_tag(self):
+        python, abi, plat = bdist_wheel.get_tag(self)
+        return "py3", "none", plat
+
+
+def generate_build_ext(pkg_dir: Path, pkg_name: str):
+    """
+    Generate extension dict for setup.py
+    the return value ext_dict, can be called in setup(**ext_dict)
+    Args:
+        pkg_dir:
+        pkg_name:
+    Returns:
+
+    """
+
+    # return dict of exts
+    return {"cmdclass": {"bdist_wheel": bdist_wheel_abi_none}}
+
+
+def set_version(pkg_dir: Path):
+    # if PYPI_VERSION does not exist, copy from VERSION
+    pypi_file = pkg_dir / "PYPI_VERSION"
+    if not pypi_file.exists():
+        with open(pkg_dir / "VERSION") as f:
+            version = f.read().strip().strip("\n")
+        with open(pypi_file, "w") as f:
+            f.write(version)
+
+
+def get_msgpack(pkg_dir: Path):
+    if not pkg_dir.is_dir():
+        raise NotImplementedError()
+
+
+def prepare_pkg(setup_file: Path) -> dict:
+    """
+
+    Args:
+        setup_file:
+    Returns:
+
+    """
+    print(f"Build wheel from {setup_file}")
+    pkg_dir = setup_file.parent
+    # package description
+    pkg_pip_name = "msgpack-cxx"
+    pkg_name = pkg_pip_name.replace("-", "_")
+    set_version(pkg_dir)
+    return generate_build_ext(pkg_dir=pkg_dir, pkg_name=pkg_name)
+
+
+setup(
+    **prepare_pkg(setup_file=Path(__file__).resolve()),
+)
