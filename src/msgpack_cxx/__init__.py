b97d29bd22a4032635a6537f0172d70888bf2f93
Tony Xiang <tony.xiang@alliander.com>
2023-09-20 13:36:25 +0200

    use pypi package:
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

---
+++ src/msgpack_cxx/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2022 Contributors to the Power Grid Model project <dynamic.grid.calculation@alliander.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from pathlib import Path
+
+def get_include() -> Path:
+    return Path(__file__).parent / "include"
