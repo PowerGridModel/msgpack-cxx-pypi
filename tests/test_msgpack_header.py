76088c0f9ccdc804980fbc5a7324ca98f0180c4a
Tony Xiang <tony.xiang@alliander.com>
2023-09-20 13:52:33 +0200

    test
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

---
+++ tests/test_msgpack_header.py
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: 2022 Contributors to the Power Grid Model project <dynamic.grid.calculation@alliander.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+
+from msgpack_cxx import get_include
+from pathlib import Path
+
+
+def test_header():
+    include_path: Path = get_include()
+    assert (include_path / "msgpack.hpp").exists()
