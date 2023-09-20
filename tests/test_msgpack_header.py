d1b57283c45653f66a9be8560504353c8dcbf089
Tony Xiang <tony.xiang@alliander.com>
2023-09-20 14:21:52 +0200

    remove header
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

--- tests/test_msgpack_header.py
+++ tests/test_msgpack_header.py
@@ -1,8 +1,3 @@
-# SPDX-FileCopyrightText: 2022 Contributors to the Power Grid Model project <dynamic.grid.calculation@alliander.com>
-#
-# SPDX-License-Identifier: MPL-2.0
-
-
 from pathlib import Path
 
 from msgpack_cxx import get_include
